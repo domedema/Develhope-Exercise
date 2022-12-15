3930b45407d5fbc5e7ff9a8f345c0586fb61417d
domedema <116555350+domedema@users.noreply.github.com>
2022-12-15 10:32:14 +0100

    Add files via upload

---
+++ es-13/exercise-3.js
@@ -0,0 +1,15 @@
+const sum =  (a, b)=> {
+  return a + b;
+}
+
+const subtract = (a, b)=> {
+  return a - b;
+}
+
+const multiply = (a, b) => a * b
+
+const divide = (a, b) => a / b
+
+const log = (value) => {
+  console.log(value);
+}
