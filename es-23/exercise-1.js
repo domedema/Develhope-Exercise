9d4cbafb1b37bf8c8dc790d4ee39e908c516c2cf
domedema <116555350+domedema@users.noreply.github.com>
2023-01-05 10:09:21 +0100

    Add files via upload

---
+++ es-23/exercise-1.js
@@ -0,0 +1,11 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+let keys= Object.keys(person)
+console.log(keys)
+let values= Object.values(person)
+console.log(values)
+// Print values of person using Object.keys
