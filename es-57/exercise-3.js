4cdd2cc1f8b59275037afe28603d24bac20d25fc
domedema <116555350+domedema@users.noreply.github.com>
2023-01-09 18:48:28 +0100

    Add files via upload

---
+++ es-57/exercise-3.js
@@ -0,0 +1,20 @@
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  static fromObject(obj = "") {
+  return obj in person;
+}
+}
+
+const obj = {
+  firstName: "Mario",
+  lastName: "Rossi",
+};
+
+
+
+
+const person = Person.fromObject(obj);
+console.log(person.firstName + " " + person.lastName);
