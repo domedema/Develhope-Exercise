98ca6df23ffc2f5f6bd47116e08ec4ea9552576b
domedema <116555350+domedema@users.noreply.github.com>
2023-01-11 11:55:40 +0100

    Add files via upload

---
+++ es-61/exercise-2.js
@@ -0,0 +1,18 @@
+//const person = {
+  //id: 1,
+ // firstName: 'Mario',
+  //lastName: 'Rossi',
+  //age: 25
+//};
+
+//const id = person.id;
+//const firstName = person.firstName;
+//const lastName = person.lastName;
+//const age = person.age;
+
+//console.log(id, firstName, lastName, age);
+
+let person = [1, "Mario", "Rossi", 25] 
+let [id,firstName,lastName,age] = person;
+
+console.log(person)
